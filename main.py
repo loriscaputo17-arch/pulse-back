d7bd80fc2c50f968c325f132841256f3dc40566a
Loris Caputo <l.caputo@quest-it.com>
2026-01-29 05:00:57 +0100

    Configure env handling and prepare for Render deploy

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
 from fastapi import FastAPI
 from app.core.gcp import setup_gcp_credentials
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.router import api_router
+
+print("GCP CREDS:", os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+print("FILE EXISTS:", os.path.exists(os.getenv("GOOGLE_APPLICATION_CREDENTIALS")))
+
 
 setup_gcp_credentials()
 app = FastAPI(title="Pulse Back")
